48b65fcc7d75b68e6126e30dd961dd1adbb1cbeb
Filipe M Hottis <lipehottis@gmail.com>
2025-02-24 09:46:09 -0300

    refactor: Deleteted any filies

--- backend/src/ schemas/product_tag.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from app.database.database import Base
-
-
-class ProductTagAssociation(Base):
-    __tablename__ = "product_tag_association"
-
-    product_id = Column(Integer, ForeignKey("products.id"), primary_key=True)
-    tag_id = Column(Integer, ForeignKey("tags.id"), primary_key=True)
