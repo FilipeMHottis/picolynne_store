cc19087e9ea81ee4e1a6fe81af0b05c74ae7b90b
Filipe M Hottis <lipehottis@gmail.com>
2025-02-24 09:47:45 -0300

    feat: :card_file_box: Creating database and backend initialization files

---
+++ backend/src/database/database.py
@@ -0,0 +1,23 @@
+from ..config.env import database
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+if database["START_DEV"]:
+    DATABASE_URL = "sqlite:///./picolynne_store_dev.db"
+else:
+    DATABASE_URL = (
+        f"postgresql://{database['DB_USER']}:"
+        f"{database['DB_PASSWORD']}@"
+        f"{database['DB_HOST']}:"
+        f"{database['DB_PORT']}/"
+        f"{database['DB_NAME']}"
+    )
+
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if database["START_DEV"] else {},
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
