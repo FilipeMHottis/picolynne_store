21f122c696e3f8d1f3199a0b4bd7d446d99597b9
Filipe Marques Hottis <lipehottis@gmail.com>
2025-04-01 14:16:20 -0300

    feat: Start Fronted

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center h-screen bg-gray-300'>
+        <h1 className=''>Test Application React With Tailwind</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
